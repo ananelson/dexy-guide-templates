cd0a30bf88145cf3321b3e04fb54e8cedc39b30a
Ana Nelson <ana@ananelson.com>
2013-12-11 17:40:03 -0800

    Updates for 1.0.0

--- dexy_guide_templates/__init__.py
+++ dexy_guide_templates/__init__.py
@@ -3,13 +3,15 @@ import os
 
 template_dir = os.path.dirname(__file__)
 
-n = len([f for f in os.listdir(template_dir) if f.endswith("-template")])
+prefixes = ['gs', 'cd']
 
-for i in xrange(n):
-    key = "gs-%02d" % i
-    if not key in Template.plugins:
-        args = {
-                'help' : "Help for %s" % key,
-                'contents-dir' : os.path.join(template_dir, "%s-template" % key)
-                }
-        Template.register_plugin(key, 'Template', args)
+for prefix in prefixes:
+    templates = [f for f in os.listdir(template_dir) if f.endswith("-template") and f.startswith(prefix)]
+    for f in templates:
+        key = f.replace("-template", "")
+        if not key in Template.plugins:
+            args = {
+                    'help' : "Help for %s" % key,
+                    'contents-dir' : os.path.join(template_dir, "%s-template" % key)
+                    }
+            Template.register_plugin(key, 'Template', args)
